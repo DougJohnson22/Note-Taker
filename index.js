1498efad17546e9023a164557e76142227f5dfab
Douglas Johnson <trunks22a@gmail.com>
2021-01-19 17:51:17 -0800

    Initial get paths established

--- index.js
+++ index.js
@@ -6,17 +6,33 @@ const path = require("path")
 
 const app = express()
 
+// Set up the port
 PORT = 8080
 
-// Use express to "get" the notes.html file
-app.get("/notes", (req,res)=>{
-    res.sendFile(path.join(__dirname, "notes.html"))
-})
+
+// Allow express to access the public folder
+app.use(express.static("public"))
+
+// Allow express to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
 // Use express to "get" the index.html file
-app.get("*", (req,res)=>{
-    res.sendFile(path.join(__dirname,"index.html"))
+app.get("/", (req, res) => {
+    res.sendFile(__dirname, "index.html")
 })
 
+// Use express to "get" the notes.html file
+app.get("/notes", (req, res) => {
+    res.sendFile(__dirname + "/public/notes.html")
+})
+
+// Use express and fs to read the JSON file and return notes
+
+// Use express and fs to receive new note and save
+
+// Use express and fs to locate and DELETE notes in db.json
 
 
 
@@ -24,8 +40,7 @@ app.get("*", (req,res)=>{
 
 
 
-
-
+// listen to the port
 app.listen(PORT, function () {
     console.log(`Listening to Port ${PORT}`)
 })
